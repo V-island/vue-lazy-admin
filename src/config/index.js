86ae15a563d98d41fe462fe27c4317e16b4f1815
Allen Li <vv15802702853@163.com>
2023-06-29 22:35:54 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -12,3 +12,39 @@ export const ROLE_LIST = 'ROLE_LIST';
 export const ROLE_LIST_INDEX = 'ROLE_LIST_INDEX';
 export const PERMISSIONS_LIST = 'PERMISSIONS';
 export const DICT_LIST = 'DICT_LIST';
+
+/* =================== 模拟用户数据 ================ */
+// 用户信息
+export const UserInfo = {
+  userId: 'zhul70',
+  userName: 'zhul70',
+  orgName: '测试组',
+  realName: '朱亮',
+};
+// 菜单信息列表
+export const MenuList = [
+  {
+    url: '/home',
+    name: '首页',
+    customIcon: '/home.png',
+  },
+];
+// 角色信息列表
+export const RoleList = [
+  {
+    userId: 'zhul70',
+    id: '8a65eac66d632e14016d6707c9de02d1',
+    code: 'IN_SYS_ADMIN',
+    name: '系统管理员',
+  },
+];
+// 权限列表
+export const PermissionsList = [
+  'ny:nyRuleHistory:list',
+  'test:onetomany:testDataMain:export',
+  'gencode:validateType:view',
+  'ureport:add',
+  'extension:actCategory:del',
+  'test:manytomany:testCourse:list',
+  'test:grid:testCountry:del',
+];

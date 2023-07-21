493f6501a0df3918932413d583af018c55979f30
Allen.Li <vv15802702853@163.com>
2023-07-21 18:16:20 +0800

    Update index.vue

--- src/views/modules/system/menu-manage/index.vue
+++ src/views/modules/system/menu-manage/index.vue
@@ -1,158 +1,147 @@
 <template>
-  <div class="mod-menu">
-    <div
-      style="display: flex; justify-content: space-between; margin-bottom: 20px"
-    >
-      <el-button
-        v-permission="'sys:menu:save'"
-        type="primary"
-        @click="addOrUpdateHandle(null)"
-        >新增菜单
-      </el-button>
-    </div>
-
-    <base-table ref="menuTable" :fetch="fetch" row-key="id" :pagination="false">
-      <el-table-column
-        prop="name"
-        header-align="center"
-        min-width="150"
-        label="名称"
-      >
-      </el-table-column>
-      <el-table-column header-align="center" align="center" label="图标">
-        <template slot-scope="scope">
-          <i :class="`fa fa-fw ${scope.row.icon || ''}`" aria-hidden="true"></i>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="type"
-        header-align="center"
-        align="center"
-        label="类型"
-      >
-        <template slot-scope="scope">
-          <el-tag v-if="scope.row.type === 0" size="small">菜单</el-tag>
-          <el-tag v-else-if="scope.row.type === 1" size="small" type="info"
-            >按钮
-          </el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="sort"
-        header-align="center"
-        align="center"
-        label="排序"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="url"
-        header-align="center"
-        align="center"
-        width="150"
-        :show-overflow-tooltip="true"
-        label="菜单URL"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="permissions"
-        header-align="center"
-        align="center"
-        width="150"
-        :show-overflow-tooltip="true"
-        label="授权标识"
-      >
-      </el-table-column>
-      <el-table-column
-        header-align="center"
-        align="center"
-        width="150"
-        label="操作"
-      >
-        <template #default="scope">
-          <el-button
-            v-permission="'sys:menu:update'"
-            type="text"
-            size="small"
-            @click="addOrUpdateHandle(scope.row.id)"
-            >修改
-          </el-button>
-          <el-button
-            v-permission="'sys:menu:delete'"
-            type="text"
-            size="small"
-            @click="deleteHandle(scope.row.id)"
-            >删除
-          </el-button>
-        </template>
-      </el-table-column>
-    </base-table>
-
-    <!-- 新增 / 修改 -->
-    <menu-add-or-update
-      :visible.sync="showDialog"
-      :menu-id="currMenuId"
-      @refreshDataList="getDataList"
-    />
-  </div>
+  <section>
+    <vxe-grid ref="table" v-bind="gridOptions"></vxe-grid>
+  </section>
 </template>
 
-<script>
-import { getAllMenu, deleteMenuByID } from 'api/system/menu-manage';
-import MenuAddOrUpdate from './menu-add-or-update';
+<script setup>
+import { ref, reactive } from 'vue';
+import XEUtils from 'xe-utils'
 
-export default {
-  name: 'menu-manage',
-  components: { MenuAddOrUpdate },
-  data() {
-    return {
-      showDialog: false,
-      currMenuId: null,
-    };
+const serveApiUrl = 'https://api.vxetable.cn/demo'
+// 创建VXETable实例
+const table = ref(null);
+// 创建VXETable配置
+const gridOptions = reactive({
+  border: true,
+  showHeaderOverflow: true,
+  showOverflow: true,
+  keepSource: true,
+  id: 'menu_table',
+  height: 730,
+  rowConfig: {
+    keyField: 'id',
+    isHover: true,
   },
-  computed: {
-    fetch() {
-      return {
-        fn: getAllMenu,
-        errorInfo: '获取菜单列表失败',
-      };
-    },
+  columnConfig: {
+    resizable: true,
   },
-  methods: {
-    // 获取数据列表
-    getDataList() {
-      this.$refs.menuTable.getTableData(true);
+  printConfig: {
+    columns: [
+      { field: 'name' },
+      { field: 'email' },
+      { field: 'nickname' },
+      { field: 'age' },
+      { field: 'amount' },
+    ],
+  },
+  pagerConfig: {
+    background: true,
+    pageSize: 10,
+    pageSizes: [5, 10, 15, 20, 50, 100, 200, 500, 1000],
+  },
+  formConfig: {
+    titleWidth: 100,
+    titleAlign: 'right',
+    items: [
+      {
+        field: 'name',
+        title: '名称',
+        span: 8,
+      },
+      {
+        span: 24,
+        align: 'center',
+        collapseNode: true,
+        itemRender: {
+          name: '$buttons',
+          children: [
+            { props: { type: 'submit', content: '搜索', status: 'primary' } },
+            { props: { type: 'reset', content: '重置' } },
+          ],
+        },
+      },
+    ],
+  },
+  toolbarConfig: {
+    buttons: [
+      { code: 'insert_actived', name: '新增' },
+      { code: 'delete', name: '直接删除' },
+      { code: 'mark_cancel', name: '删除/取消' },
+      { code: 'save', name: '保存', status: 'primary' },
+    ],
+  },
+  proxyConfig: {
+    seq: true, // 启用动态序号代理，每一页的序号会根据当前页数变化
+    sort: true, // 启用排序代理，当点击排序时会自动触发 query 行为
+    filter: true, // 启用筛选代理，当点击筛选时会自动触发 query 行为
+    form: true, // 启用表单代理，当点击表单提交按钮时会自动触发 reload 行为
+    props: {
+      // 对应响应结果 Promise<{ result: [], page: { total: 100 } }>
+      result: 'result', // 配置响应结果列表字段
+      total: 'page.total', // 配置响应结果总页数字段
     },
-    // 新增 / 修改
-    addOrUpdateHandle(id) {
-      this.currMenuId = id;
-      this.showDialog = true;
-    },
-    // 删除
-    deleteHandle(id) {
-      this.$confirm(`确定对[id=${id}]进行[删除]操作?`, '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning',
-      }).then(async () => {
-        const [err, data] = await this._awaitWrap(deleteMenuByID(id));
-        if (err) {
-          this.$message.error('菜单删除失败');
-          return;
+    // 只接收Promise，具体实现自由发挥
+    ajax: {
+      // 当点击工具栏查询按钮或者手动提交指令 query或reload 时会被触发
+      query: ({ page, sorts, filters, form }) => {
+        const queryParams = Object.assign({}, form);
+        // 处理排序条件
+        const firstSort = sorts[0];
+        if (firstSort) {
+          queryParams.sort = firstSort.field;
+          queryParams.order = firstSort.order;
         }
-        if (data && data.code === 0) {
-          this.getDataList();
-          this.$message.success('菜单删除成功');
-        } else {
-          this.$message.error(data.msg);
-        }
-      });
+        // 处理筛选条件
+        filters.forEach(({ field, values }) => {
+          queryParams[field] = values.join(',');
+        });
+        return fetch(
+          `${serveApiUrl}/api/pub/page/list/${page.pageSize}/${
+            page.currentPage
+          }?${XEUtils.serialize(queryParams)}`,
+        ).then((response) => response.json());
+      },
+      // 当点击工具栏删除按钮或者手动提交指令 delete 时会被触发
+      delete: ({ body }) => {
+        return fetch(`${serveApiUrl}/api/pub/save`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        }).then((response) => response.json());
+      },
+      // 当点击工具栏保存按钮或者手动提交指令 save 时会被触发
+      save: ({ body }) => {
+        return fetch(`${serveApiUrl}/api/pub/save`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        }).then((response) => response.json());
+      },
     },
   },
-};
+  columns: [
+    {
+      field: 'title',
+      title: '名称',
+    },{
+      field: 'icon',
+      title: '图标',
+    },{
+      field: 'type',
+      title: '类型',
+    },{
+      field: 'sort',
+      title: '排序',
+    },{
+      field: 'url',
+      title: '菜单URL',
+    },{
+      field: 'permissions',
+      title: '授权标识',
+    },
+  ],
+});
 </script>
-<style lang="scss" scoped>
-@import '~@/styles/_common.scss';
-.mod-menu {
-  @extend %wh-full;
-}
-</style>
+
+<style lang="scss" scoped></style>
